bf315927a43ebbb3511027de26f3cc35bbfcd082
Dmitri Dimitrioglo <ddimitrioglo@goparrot.ai>
2021-03-10 14:33:07 +0200

    feat: use lodash.cloneDeep

--- util.ts
+++ util.ts
@@ -1,3 +1,5 @@
+const cloneDeep = require('lodash.clonedeep');
+
 export const hasOwnProperty = Object.prototype.hasOwnProperty
 
 export function objectType(object: any) {
@@ -19,38 +21,6 @@ function isNonPrimitive(value: any): value is object {
   return value != null && typeof value == 'object'
 }
 
-/**
-Recursively copy a value.
-
-@param source - should be a JavaScript primitive, Array, or (plain old) Object.
-@returns copy of source where every Array and Object have been recursively
-         reconstructed from their constituent elements
-*/
 export function clone<T extends any>(source: T): T {
-  if (!isNonPrimitive(source)) {
-    // short-circuiting is faster than a single return
-    return source
-  }
-  // x.constructor == Array is the fastest way to check if x is an Array
-  if (source.constructor == Array) {
-    // construction via imperative for-loop is faster than source.map(arrayVsObject)
-    const length = (source as Array<any>).length
-    // setting the Array length during construction is faster than just `[]` or `new Array()`
-    const arrayTarget: any = new Array(length)
-    for (let i = 0; i < length; i++) {
-      arrayTarget[i] = clone(source[i])
-    }
-    return arrayTarget
-  }
-  // Object
-  const objectTarget: any = {}
-  // declaring the variable (with const) inside the loop is faster
-  for (const key in source) {
-    // hasOwnProperty costs a bit of performance, but it's semantically necessary
-    // using a global helper is MUCH faster than calling source.hasOwnProperty(key)
-    if (hasOwnProperty.call(source, key)) {
-      objectTarget[key] = clone(source[key])
-    }
-  }
-  return objectTarget
+  return cloneDeep(source);
 }
